6bc324e3ec4b3a732d22589a03526a028cb88486
Joel Christner <joel.christner@gmail.com>
2018-03-26 13:34:27 -0700

    Bash scripts in SampleFiles

---
+++ Server/SampleFiles/parse_preview.sh
@@ -0,0 +1,1 @@
+curl -H "x-api-key: default" -d "@order1.json" -X POST "http://localhost:9090/_parse?type=json&pretty=true"
---
+++ Server/SampleFiles/prepare.sh
@@ -0,0 +1,17 @@
+# delete the 'First' index
+curl -H "x-api-key: default" -X DELETE "http://localhost:9090/First?cleanup=true"
+
+# create the 'First' index
+curl -H "x-api-key: default" -d "@index.json" -X POST "http://localhost:9090/indices" 
+
+# add three JSON files to the 'First' index
+curl -H "x-api-key: default" -d "@order1.json" -X POST "http://localhost:9090/First/document?type=json"
+curl -H "x-api-key: default" -d "@order2.json" -X POST "http://localhost:9090/First/document?type=json"
+curl -H "x-api-key: default" -d "@order3.json" -X POST "http://localhost:9090/First/document?type=json"
+
+# submit query 1
+curl -H "x-api-key: default" -d "@query1.json" -X PUT "http://localhost:9090/First"
+
+# retrieve source documents (modify with the appropriate document ID)
+curl -H "x-api-key: default" "http://localhost:9090/First/7b61305d-cb30-469a-87f8-83dea2724f73"
+curl -H "x-api-key: default" "http://localhost:9090/First/7b61305d-cb30-469a-87f8-83dea2724f73?parsed=true&pretty=true"
